ddef83a314a870cbf33286080e1c88a07105f91a
alex <alex.moik@gmx.at>
2024-12-02 16:45:29 +0100

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/MatrixCipher/',  // Dies muss der Name deines GitHub-Repositories sein
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
   server: {
     port: 5173
   }
